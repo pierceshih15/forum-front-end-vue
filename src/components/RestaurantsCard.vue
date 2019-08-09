50ebaa8e0bfee354049ede7e7dce62f4ee1c911a
pierce <pierceshih@gmail.com>
2019-08-09 19:18:04 +0800

    Updated Restaurants page and RestaurantFeeds page, also the related feautures.

--- src/components/RestaurantsCard.vue
+++ src/components/RestaurantsCard.vue
@@ -20,25 +20,25 @@
           v-if="restaurant.isFavorited"
           type="button"
           class="btn btn-danger btn-border favorite mr-2"
-          @click.stop.prevent="deleteFavorite"
+          @click.stop.prevent="deleteFavorite(restaurant.id)"
         >移除最愛</button>
         <button
           v-else
           type="button"
           class="btn btn-primary btn-border favorite mr-2"
-          @click.stop.prevent="addFavorite"
+          @click.stop.prevent="addFavorite(restaurant.id)"
         >加到最愛</button>
         <button
           v-if="restaurant.isLiked"
           type="button"
           class="btn btn-danger like mr-2"
-          @click.stop.prevent="deleteLike"
+          @click.stop.prevent="deleteLike(restaurant.id)"
         >Unlike</button>
         <button
           v-else
           type="button"
           class="btn btn-primary like mr-2"
-          @click.stop.prevent="addLike"
+          @click.stop.prevent="addLike(restaurant.id)"
         >Like</button>
       </div>
     </div>
@@ -46,6 +46,9 @@
 </template>
 
 <script>
+import usersAPI from "./../apis/users";
+import { Toast } from "./../utils/helpers";
+
 export default {
   props: {
     initialRestaurant: {
@@ -59,29 +62,89 @@ export default {
     };
   },
   methods: {
-    addFavorite() {
-      this.restaurant = {
-        ...this.restaurant,
-        isFavorited: true
-      };
+    async addFavorite(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.addFavorite({
+          restaurantId
+        });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          isFavorited: true
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法將餐廳加入最愛，請稍後再試"
+        });
+      }
     },
-    deleteFavorite() {
-      this.restaurant = {
-        ...this.restaurant,
-        isFavorited: false
-      };
+    async deleteFavorite(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.deleteFavorite({
+          restaurantId
+        });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          isFavorited: false
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法將餐廳移除最愛，請稍後再試"
+        });
+      }
     },
-    addLike() {
-      this.restaurant = {
-        ...this.restaurant,
-        isLiked: true
-      };
+    async addLike(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.addLike({
+          restaurantId
+        });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          isLiked: true
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法按讚，請稍後再試"
+        });
+      }
     },
-    deleteLike() {
-      this.restaurant = {
-        ...this.restaurant,
-        isLiked: false
-      };
+    async deleteLike(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.deleteLike({
+          restaurantId
+        });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          isLiked: false
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法取消按讚，請稍後再試"
+        });
+      }
     }
   }
 };
--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -32,279 +32,8 @@ import RestaurantsCard from "./../components/RestaurantsCard";
 import RestaurantsNavPills from "./../components/RestaurantsNavPills";
 import RestaurantsPagination from "./../components/RestaurantsPagination";
 
-const dummyData = {
-  restaurants: [
-    {
-      id: 30,
-      name: "Lyda Bruen",
-      tel: "1-295-307-9128 x25682",
-      address: "79554 Kassulke Radial",
-      opening_hours: "08:00",
-      description: "Et quam iste nulla ut et distinctio sunt omnis rec",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=25.932072460400413",
-      viewCounts: 1,
-      createdAt: "2019-07-30T16:24:55.440Z",
-      updatedAt: "2019-07-31T04:26:33.945Z",
-      CategoryId: 1,
-      Category: {
-        id: 1,
-        name: "中式料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 27,
-      name: "Penelope Mayert DDS",
-      tel: "1-775-369-6229",
-      address: "52148 Susan Pike",
-      opening_hours: "08:00",
-      description: "non nobis quo",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=77.93437663958767",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.439Z",
-      updatedAt: "2019-07-30T16:24:55.439Z",
-      CategoryId: 1,
-      Category: {
-        id: 1,
-        name: "中式料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 26,
-      name: "Zoe Connelly",
-      tel: "724-565-0111",
-      address: "8718 Nolan Roads",
-      opening_hours: "08:00",
-      description: "Ipsum aspernatur quis est voluptates maiores quam ",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=51.62937107379864",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.438Z",
-      updatedAt: "2019-07-30T16:24:55.438Z",
-      CategoryId: 1,
-      Category: {
-        id: 1,
-        name: "中式料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 24,
-      name: "Al Von",
-      tel: "038-326-5052",
-      address: "5559 VonRueden Courts",
-      opening_hours: "08:00",
-      description: "Aliquid numquam hic. Minima perferendis quia cum t",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=37.9512586281334",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.438Z",
-      updatedAt: "2019-07-30T16:24:55.438Z",
-      CategoryId: 1,
-      Category: {
-        id: 1,
-        name: "中式料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 23,
-      name: "Obie Veum",
-      tel: "852.152.3521 x47973",
-      address: "543 Waters Bypass",
-      opening_hours: "08:00",
-      description: "Ea voluptate fugiat id est voluptatem sit. Aut deb",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=47.513014434319146",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.437Z",
-      updatedAt: "2019-07-30T16:24:55.437Z",
-      CategoryId: 1,
-      Category: {
-        id: 1,
-        name: "中式料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 17,
-      name: "Alexandro Bahringer",
-      tel: "293-115-4784",
-      address: "75754 Ashlynn Field",
-      opening_hours: "08:00",
-      description: "Et dolores non harum. Non autem dolorem deserunt. ",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=46.018008252855424",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.436Z",
-      updatedAt: "2019-07-30T16:24:55.436Z",
-      CategoryId: 1,
-      Category: {
-        id: 1,
-        name: "中式料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 7,
-      name: "Melvin Flatley Sr.",
-      tel: "1-255-894-3279 x2314",
-      address: "138 Quinn Street",
-      opening_hours: "08:00",
-      description: "quos dolore tenetur",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=43.25768589113377",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.434Z",
-      updatedAt: "2019-07-30T16:24:55.434Z",
-      CategoryId: 1,
-      Category: {
-        id: 1,
-        name: "中式料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 46,
-      name: "Jasper Mueller",
-      tel: "254-159-2190",
-      address: "44890 Dominique Wall",
-      opening_hours: "08:00",
-      description: "dolorem ratione et",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=67.08831513935154",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.443Z",
-      updatedAt: "2019-07-30T16:24:55.443Z",
-      CategoryId: 2,
-      Category: {
-        id: 2,
-        name: "日本料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 42,
-      name: "Melody Koepp",
-      tel: "(774) 342-2533",
-      address: "3318 Feeney Hollow",
-      opening_hours: "08:00",
-      description: "beatae iste alias",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=71.34426799459621",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.442Z",
-      updatedAt: "2019-07-30T16:24:55.442Z",
-      CategoryId: 2,
-      Category: {
-        id: 2,
-        name: "日本料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 40,
-      name: "Robyn Schroeder",
-      tel: "406.132.4400",
-      address: "647 Yundt Mountains",
-      opening_hours: "08:00",
-      description: "Incidunt rerum perspiciatis ut iste ipsum labore.",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=15.902129223636363",
-      viewCounts: null,
-      createdAt: "2019-07-30T16:24:55.441Z",
-      updatedAt: "2019-07-30T16:24:55.441Z",
-      CategoryId: 2,
-      Category: {
-        id: 2,
-        name: "日本料理",
-        createdAt: "2019-07-30T16:24:55.429Z",
-        updatedAt: "2019-07-30T16:24:55.429Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    }
-  ],
-  categories: [
-    {
-      id: 1,
-      name: "中式料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    },
-    {
-      id: 2,
-      name: "日本料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    },
-    {
-      id: 3,
-      name: "義大利料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    },
-    {
-      id: 4,
-      name: "墨西哥料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    },
-    {
-      id: 5,
-      name: "素食料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    },
-    {
-      id: 6,
-      name: "美式料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    },
-    {
-      id: 7,
-      name: "複合式料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z"
-    }
-  ],
-  categoryId: "",
-  page: 1,
-  totalPage: [1, 2, 3, 4, 5],
-  prev: 1,
-  next: 2
-};
+import restaurantsAPI from "./../apis/restaurants";
+import { Toast } from "./../utils/helpers";
 
 export default {
   components: {
@@ -317,21 +46,52 @@ export default {
     return {
       restaurants: [],
       categories: [],
-      categoryId: -1,
+      categoryId: 1,
       currentPage: 1,
       totalPage: -1
     };
   },
   created() {
-    this.fetchRestaurants();
+    const { page, categoryId } = this.$route.query;
+    this.fetchRestaurants({
+      page,
+      categoryId
+    });
+  },
+  beforeRouteUpdate(to, from, next) {
+    const { page, categoryId } = to.query;
+    this.fetchRestaurants({ page, categoryId });
+    next();
   },
   methods: {
-    fetchRestaurants() {
-      this.restaurants = dummyData.restaurants;
-      this.categories = dummyData.categories;
-      this.categoryId = dummyData.categoryId;
-      this.currentPage = dummyData.page;
-      this.totalPage = dummyData.totalPage.length;
+    async fetchRestaurants({ page = 1, categoryId = "" }) {
+      try {
+        // STEP 1：串接 api 回傳 response 資料
+        const response = await restaurantsAPI.getRestaurants({
+          page,
+          categoryId
+        });
+
+        // STEP 2：將 response 中的 data 和 statusText 取出
+        const { data, statusText } = response;
+
+        // STEP 3：如果 statusText 不是 OK 的話則進入錯誤處理
+        if (statusText !== "OK") {
+          throw new Error(statusText);
+        }
+
+        // STEP 4：將從伺服器取得的 data 帶入 Vue 內
+        this.restaurants = data.restaurants;
+        this.categories = data.categories;
+        this.categoryId = data.categoryId;
+        this.currentPage = data.page;
+        this.totalPage = data.totalPage.length;
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法取得餐廳資料，請稍後再試"
+        });
+      }
     }
   }
 };
